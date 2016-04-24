4e8dc718e67a611aa0d03f38a53800664a224f1a
isirichard <isi.12ichard@gmail.com>
2016-04-23 23:07:45 -0500

    al 90%funcionalidad

--- src/epis/unsa/RegistroServlet.java
+++ src/epis/unsa/RegistroServlet.java
@@ -11,13 +11,26 @@ import java.util.ArrayList;
 public class RegistroServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		
-		
-		ArrayList<Persona>poblacion0=new ArrayList<Persona>();
-		getServletContext().setAttribute("poblacion", poblacion0);
+		//preguntando si la variable no existe para crearla.
+		if(getServletContext().getAttribute("poblacion")==null){
+			//Creando arreglo para la clase persona
+			ArrayList<Persona>poblacion1=new ArrayList<Persona>();
+
+			//añadiendo datos al arreglo persona.
+			Persona persona1=new Persona(72126234,"DIEGO","OVIEDO YAURI","No es Miembro de Mesa","301285","IE 104 LA RECOLETA","400","2","10","PASAJE LA RECOLETA");
+			//persona1={72126234,"DIEGO","OVIEDO YAURI",301285,"IE 104 LOS PANMCHITOS",400,2,10,"cALLE OLVIDO"};
+			Persona persona2=new Persona(73868584,"AUDREY","TACCA BARRANTES","Si es Miembro de Mesa","355623","IE LOS PANDAS","200","3","1","CALLE PERAL 507 ");
+			Persona persona3=new Persona(45655339,"RICHARD","ALVAREZ MAMANI","No es Miembro de Mesa","234533","UNIVERSIDAD WOW","100","2","3","CALLE LEGION 107");
+			
+			poblacion1.add(persona1);
+			poblacion1.add(persona2);
+			poblacion1.add(persona3);
+			//==>PARTE IMPORTANTE Guardando arreglo poblacion en el conteiner con nombre población
+			//getServletContext().setAttribute("NombreVariableParaGuardarConteiner", VaribaleAGuardar);
+			getServletContext().setAttribute("poblacion", poblacion1);
+		}
 		
 		ArrayList<Persona>poblacion=(ArrayList<Persona>)getServletContext().getAttribute("poblacion");
-		getServletContext().setAttribute("poblacion", poblacion);
 		
 		String dninum=req.getParameter("dni123");
 		int dniNum=Integer.parseInt(dninum);
@@ -52,9 +65,9 @@ public class RegistroServlet extends HttpServlet {
 				+ "<title>AGREGADO CORRECTAMENTE</title>"
 				+ "</head>"
 				+ "<body>"
-				+ "<a href='index.html'>BUSCAR</a>"
-				+ "<a href='registro.jsp'>REGISTRAR</a>"
-				+ "<a href='index.html'>VER</a>"
+				+ "<a href='index.html'>BUSCAR </a>"
+				+ "<a href='registro.jsp'>REGISTRAR </a>"
+				+ "<a href='index.html'>VER </a>"
 				+ "<h1>AGREGADO CORRECTAMENTE>"
 				+ "<p>"+dniNum+" "+nombre+" "+apellido+" "+esMiembro+" "+mesa+" "+local+" "+pabellon+" "+piso+" "+aula+" "+direccion
 				+ "</p></body></html>");
--- src/epis/unsa/VerServlet.java
+++ src/epis/unsa/VerServlet.java
@@ -11,9 +11,27 @@ import javax.servlet.http.HttpServletResponse;
 public class VerServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		//ArrayList<Persona>poblacion=(ArrayList<Persona>)getServletContext().getAttribute("poblacion");
 		
+		//preguntando si la variable no existe para crearla.
+		if(getServletContext().getAttribute("poblacion")==null){
+			//Creando arreglo para la clase persona
+			ArrayList<Persona>poblacion1=new ArrayList<Persona>();
+
+			//añadiendo datos al arreglo persona.
+			Persona persona1=new Persona(72126234,"DIEGO","OVIEDO YAURI","No es Miembro de Mesa","301285","IE 104 LA RECOLETA","400","2","10","PASAJE LA RECOLETA");
+			//persona1={72126234,"DIEGO","OVIEDO YAURI",301285,"IE 104 LOS PANMCHITOS",400,2,10,"cALLE OLVIDO"};
+			Persona persona2=new Persona(73868584,"AUDREY","TACCA BARRANTES","Si es Miembro de Mesa","355623","IE LOS PANDAS","200","3","1","CALLE PERAL 507 ");
+			Persona persona3=new Persona(45655339,"RICHARD","ALVAREZ MAMANI","No es Miembro de Mesa","234533","UNIVERSIDAD WOW","100","2","3","CALLE LEGION 107");
+			
+			poblacion1.add(persona1);
+			poblacion1.add(persona2);
+			poblacion1.add(persona3);
+			//==>PARTE IMPORTANTE Guardando arreglo poblacion en el conteiner con nombre población
+			//getServletContext().setAttribute("NombreVariableParaGuardarConteiner", VaribaleAGuardar);
+			getServletContext().setAttribute("poblacion", poblacion1);
+		}
 		
+		ArrayList<Persona>poblacion=(ArrayList<Persona>)getServletContext().getAttribute("poblacion");	
 		resp.setContentType("text/html");
 		PrintWriter out=resp.getWriter();
 		
@@ -22,9 +40,12 @@ public class VerServlet extends HttpServlet {
 		+"<head>"
 			+"<meta charset='UTF-8'>"
 			+"<title>VerLista</title>"
-			+"<link rel='stylesheet' type='text/css' href='tablaCss.css'>"
+			+"<link rel='stylesheet' type='text/css' href='../css/onpe.css'>"
 		+"</head>"
 		+"<body>"
+			+"<a href='index.html'>BUSCAR </a>"
+			+"<a href='registro.jsp'>REGISTRAR </a>"
+			+"<a href='/ver'>VER </a>"
 			+"<div id='contenedor'>"
 		    +"<div id='contenidos'>"
 		    	+"<div class='columna'>DNI</div>"
@@ -39,23 +60,27 @@ public class VerServlet extends HttpServlet {
 		        +"<div class='columna'>Dirección</div>"
 		    +"</div>"
 				);   
-		    
+		for(int i=0;i<poblacion.size();i++){
+			out.println(
+				    "<div id='contenidos'>"
+			    		+"<div class='columna'>"+poblacion.get(i).getDni()+"</div>"
+				        +"<div class='columna'>"+poblacion.get(i).getNombre()+"</div>"
+				        +"<div class='columna'>"+poblacion.get(i).getApellidos()+"</div>"
+				        +"<div class='columna'>"+poblacion.get(i).getMienbroMesa()+"</div>"
+				        +"<div class='columna'>"+poblacion.get(i).getMesaSufragio()+"</div>"
+				        +"<div class='columna'>"+poblacion.get(i).getLocalVotacion()+"</div>"
+				        +"<div class='columna'>"+poblacion.get(i).getPabellon()+"</div>"
+				        +"<div class='columna'>"+poblacion.get(i).getPiso()+"</div>"
+				        +"<div class='columna'>"+poblacion.get(i).getAula()+"</div>"
+				        +"<div class='columna'>"+poblacion.get(i).getDireccion()+"</div>"
+				    +"</div>"
+					); 
+		}
+					
 		out.println(
-		    "<div id='contenidos'>"
-	    		+"<div class='columna'>DNI</div>"
-		        +"<div class='columna'>Nombres</div>"
-		        +"<div class='columna'>Apellidos</div>"
-		        +"<div class='columna'>MienbroDeMesa</div>"
-		        +"<div class='columna'>MesaSufragio</div>"
-		        +"<div class='columna'>LocalVotación</div>"
-		        +"<div class='columna'>Pabellon</div>"
-		        +"<div class='columna'>Piso</div>"
-		        +"<div class='columna'>Aula</div>"
-		        +"<div class='columna'>Dirección</div>"
-		    +"</div>"
-		+"</div>"
-		+"</body>"
-		+"</html>"
+				"</div>"
+				+"</body>"
+				+"</html>"
 				); 
 		
 	}
